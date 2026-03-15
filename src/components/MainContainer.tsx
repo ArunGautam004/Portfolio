01c05fef6e5934a1f9b362534b9e1d192da3797b
Arun0041 <arungautam0041@gmail.com>
2026-03-16 00:35:54 +0530

    add lenis smooth scroll

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -9,6 +9,7 @@ import SocialIcons from "./SocialIcons";
 import WhatIDo from "./WhatIDo";
 import Work from "./Work";
 import setSplitText from "./utils/splitText";
+import "./styles/mobile.css";
 
 const TechStack = lazy(() => import("./TechStack"));
 
